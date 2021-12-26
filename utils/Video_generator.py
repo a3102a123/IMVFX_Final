91b3b1a5f0a372c0faa9da20552a63f42bae7a15
karta2155802 <karta2155802@gmail.com>
2021-12-27 01:09:25 +0800

    update video_generator

--- utils/Video_generator.py
+++ utils/Video_generator.py
@@ -15,6 +15,8 @@ parser.add_argument("--width", default=960, type=int,
                     help='The width of video frame image.')
 parser.add_argument("--height", default=536, type=int,
                     help='The height of video frame image.')
+parser.add_argument("--frame_num", default=0, type=int,
+                    help='The number of image.')
 
 args, unknown = parser.parse_known_args()
 
@@ -23,17 +25,15 @@ Output = args.output
 FPS = args.fps
 Width = args.width
 Height = args.height
+Frame_num = args.frame_num
 codec = cv2.VideoWriter_fourcc(*'XVID')
 out_vid = cv2.VideoWriter(Output, codec, FPS, (Width, Height))
 
 if __name__ == "__main__":
     if(os.path.isdir(Frame_Dir)):
-
-        frame_list = os.listdir(Frame_Dir)
-        frame_list = sorted(frame_list)
-        for f in frame_list:
-            img_path = os.path.join(Frame_Dir,f)
-            img = cv2.imread(img_path)
+        for f in [r'{}inpainting_{}.png'.format(Frame_Dir, i) for i in range(Frame_num)]:
+            #img_path = os.path.join(Frame_Dir,f)
+            img = cv2.imread(f)
             out_vid.write(img)
             print(f)
         out_vid.release()
